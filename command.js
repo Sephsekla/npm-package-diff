25b38c498085ca8dfbb19fc7b743703c5dd002cb
Joe Bailey-Roberts <joe.br@humanmade.com>
2023-06-23 16:14:36 +0100

    Restructure file

--- command.js
+++ command.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
-var _a, _b;
 var spawnSync = require('node:child_process').spawnSync;
 var parsedArgs = require('minimist');
 ;
 var executeStep = function (cmd, args) {
     var command = spawnSync(cmd, args, { encoding: 'utf-8' });
+    if (command.status > 0) {
+        console.error(command.stderr);
+        process.exit(1);
+    }
     return command.stdout;
 };
-var args = parsedArgs(process.argv.slice(2));
-var base = (_b = (_a = args.base) !== null && _a !== void 0 ? _a : args.b) !== null && _b !== void 0 ? _b : 'HEAD';
-var baselineLockfile = executeStep('git', ['show', "".concat(base, ":package-lock.json")]);
-console.log(JSON.parse(baselineLockfile));
+var getBaseLineLockfile = function (base) {
+    var command = executeStep('git', ['show', "".concat(base, ":package-lock.json")]);
+    return JSON.parse(command);
+};
+var run = function () {
+    var _a, _b;
+    var args = parsedArgs(process.argv.slice(2));
+    var base = (_b = (_a = args.base) !== null && _a !== void 0 ? _a : args.b) !== null && _b !== void 0 ? _b : 'HEAD';
+    var baselineLockfile = getBaseLineLockfile(base);
+    console.log(baselineLockfile);
+};
+run();
--- command.ts
+++ command.ts
@@ -12,16 +12,28 @@ interface Arguments {
 const executeStep = ( cmd: string, args?: string[] ) => {
 	
 	const command =  spawnSync( cmd, args, { encoding: 'utf-8' } );
+	
+	if( command.status > 0 ) {
+		console.error(command.stderr);
+		process.exit(1);
+	}
+
 	return command.stdout;
 }
 
-const getBaseLineLockfile = () => {
+const getBaseLineLockfile = ( base: string ) => {
 	const command = executeStep('git', ['show', `${base}:package-lock.json`]);
 	return JSON.parse( command );
 }
 
-const args: Arguments = parsedArgs(process.argv.slice(2));
+const run = () => {
 
-const base: string = args.base ?? args.b ?? 'HEAD';
+	const args: Arguments = parsedArgs(process.argv.slice(2));
+	const base: string = args.base ?? args.b ?? 'HEAD';
 
-const baselineLockfile = getBaseLineLockfile();
+	const baselineLockfile = getBaseLineLockfile( base );
+
+	console.log(baselineLockfile);
+}
+
+run();
